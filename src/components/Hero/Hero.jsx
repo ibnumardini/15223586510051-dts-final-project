f1761005d59a1fe3d832b9e7947095fce8276cc1
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2022-07-30 23:59:37 +0700

    fix: change hero api to news api

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,17 +1,16 @@
 import { Row, Col, Carousel } from "antd";
 import moment from "moment";
-import { Link } from "react-router-dom";
 
-import { useGetLatestNewsQuery } from "../../services/news/cnbc";
+import { useGetNewsQuery } from "../../services/news/cnbc";
 import { Toast, ToastError } from "../../utils/Notification";
 import Loading from "../../utils/Loading";
 
 import styles from "./Hero.module.css";
 
 export const Hero = () => {
-  const { data, error, isLoading } = useGetLatestNewsQuery();
+  const { data, error, isLoading } = useGetNewsQuery();
 
-  const latestNews = data?.data?.posts?.slice(0, 5);
+  const news = data?.data?.posts?.slice(0, 5);
 
   return (
     <>
@@ -24,7 +23,7 @@ export const Hero = () => {
           <Col md={1} lg={2} xxl={4}></Col>
           <Col md={22} lg={20} xxl={16}>
             <Carousel autoplay>
-              {latestNews.map((val, idx) => {
+              {news.map((val, idx) => {
                 return <HeroElement key={idx} post={val} />;
               })}
             </Carousel>
@@ -40,12 +39,16 @@ const HeroElement = ({ post }) => {
   return (
     <div
       className={styles.hero}
-      style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.5)), url(${post.thumbnail})` }}
+      style={{
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.5)), url(${post.thumbnail})`,
+      }}
     >
       <div className={styles.hero__box}>
         <div className={styles.hero__content}>
           <span className={styles.hero__title} level={2}>
-            <a href={post.link} target="_blank">{post.title}</a>
+            <a href={post.link} target="_blank">
+              {post.title}
+            </a>
           </span>
           <span className={styles.hero__subtitle} level={4}>
             {post.description}

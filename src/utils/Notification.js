851fb25f02fb13cf3de6ec5c29b90e0cffd606a8
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2022-08-04 21:09:31 +0700

    fix: config toast notification

--- src/utils/Notification.js
+++ src/utils/Notification.js
@@ -4,6 +4,10 @@ export const ToastSuccess = "success";
 export const ToastError = "error";
 
 export const Toast = (type, { title, desc = "" }) => {
+  notification.config({
+    placement: "bottomLeft",
+    duration: 1,
+  });
   notification[type]({
     message: title,
     description: desc,
